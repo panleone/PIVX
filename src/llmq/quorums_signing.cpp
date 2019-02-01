835ce431f4d8ab0d5ec0cb632a1b97e2a653c8ac
Alexander Block <ablock84@gmail.com>
2019-02-01 08:59:10 +0100

    Introduce startBlockHeight to make things more explicit

--- src/llmq/quorums_signing.cpp
+++ src/llmq/quorums_signing.cpp
@@ -558,10 +558,11 @@ CQuorumCPtr CSigningManager::SelectQuorumForSigning(Consensus::LLMQType llmqType
     CBlockIndex* pindexStart;
     {
         LOCK(cs_main);
-        if (signHeight - SIGN_HEIGHT_OFFSET > chainActive.Height()) {
+        int startBlockHeight = signHeight - SIGN_HEIGHT_OFFSET;
+        if (startBlockHeight > chainActive.Height()) {
             return nullptr;
         }
-        pindexStart = chainActive[signHeight - SIGN_HEIGHT_OFFSET];
+        pindexStart = chainActive[startBlockHeight];
     }
 
     auto quorums = quorumManager->ScanQuorums(llmqType, pindexStart, poolSize);
