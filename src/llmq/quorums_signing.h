af7bb99ddc0977e7154700c08377b80980f2795d
Alexander Block <ablock84@gmail.com>
2019-08-12 09:36:09 +0200

    Re-verify invalid IS sigs when the active quorum set rotated (https://github.com/dashpay/dash/pull/3052)
    * Split ProcessPendingInstantSendLocks into two methods
    
    * Split SelectQuorumForSigning into SelectQuorumForSigning and GetActiveQuorumSet
    
    * Implement retrying of IS lock verification when the LLMQ active set rotates

--- src/llmq/quorums_signing.h
+++ src/llmq/quorums_signing.h
@@ -160,6 +160,8 @@ public:
     bool IsConflicting(Consensus::LLMQType llmqType, const uint256& id, const uint256& msgHash);
     bool HasVotedOnId(Consensus::LLMQType llmqType, const uint256& id);
     bool GetVoteForId(Consensus::LLMQType llmqType, const uint256& id, uint256& msgHashRet);
+
+    std::vector<CQuorumCPtr> GetActiveQuorumSet(Consensus::LLMQType llmqType, int signHeight);
     CQuorumCPtr SelectQuorumForSigning(Consensus::LLMQType llmqType, int signHeight, const uint256& selectionHash);
     // Verifies a recovered sig that was signed while the chain tip was at signedAtTip
     bool VerifyRecoveredSig(Consensus::LLMQType llmqType, int signedAtHeight, const uint256& id, const uint256& msgHash, const CBLSSignature& sig);
