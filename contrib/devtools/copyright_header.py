22889ee81ef9039d8ad4e53e2fb9a37e0a63df9e
gchuf <gasper.cefarin@gmail.com>
2019-10-04 17:47:11 +0200

    scripts: update copyright_header script to include additional files
    Includes .sh and .bash-completion files in the script as well

--- contrib/devtools/copyright_header.py
+++ contrib/devtools/copyright_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2016-2018 The Bitcoin Core developers
+# Copyright (c) 2016-2019 The Bitcoin Core developers
 # Copyright (c) 2018-2019 The PIVX developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -42,7 +42,7 @@ EXCLUDE_DIRS = [
     "src/rust",
 ]
 
-INCLUDE = ['*.h', '*.cpp', '*.cc', '*.c', '*.mm', '*.py']
+INCLUDE = ['*.h', '*.cpp', '*.cc', '*.c', '*.mm', '*.py', '*.sh', '*.bash-completion']
 INCLUDE_COMPILED = re.compile('|'.join([fnmatch.translate(m) for m in INCLUDE]))
 
 def applies_to_file(filename):
