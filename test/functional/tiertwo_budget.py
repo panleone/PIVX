0245001eb410a5d382e4947751a00b4fe7876ca0
random-zebra <random.zebra@protonmail.com>
2022-03-03 22:44:10 +0100

    [FIX] Don't use miner to receive budget proposals. Use an owner.

--- test/functional/tiertwo_budget.py
+++ test/functional/tiertwo_budget.py
@@ -86,7 +86,7 @@ class BudgetTest(PivxTier2TestFramework):
         assert_equal(voteResult["detail"][0]["result"], "success")
 
     def check_address_balance(self, addr, expected_balance, has_balance=True):
-        addrInfo = self.miner.listreceivedbyaddress(0, False, False, addr)
+        addrInfo = self.nodes[self.ownerOnePos].listreceivedbyaddress(0, False, False, addr)
         if has_balance:
             assert_equal(addrInfo[0]["amount"], expected_balance)
         else:
@@ -146,7 +146,7 @@ class BudgetTest(PivxTier2TestFramework):
             props.append(Proposal("prop_"+str(i),
                          "https://link_"+str(i)+".com",
                          3,
-                         self.miner.getnewaddress(),
+                         self.nodes[self.ownerOnePos].getnewaddress(),
                          11 * (i + 1)))
         self.submit_proposals(props)
 
