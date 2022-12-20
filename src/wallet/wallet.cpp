6855e3dda2a135e6132ede155bb08fd106a5ff71
Alessandro Rezzi <alessandro.rezzi@studenti.unipd.it>
2022-12-20 16:02:13 +0100

    on encryption the seed is no longer changed, but a new keypool is generated

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -830,9 +830,9 @@ bool CWallet::EncryptWallet(const SecureString& strWalletPassphrase)
 
         Lock();
         Unlock(strWalletPassphrase);
-        // if we are using HD, replace the HD seed with a new one
+        // if we are using HD, empty the keypool and generate new keys
         if (m_spk_man->IsHDEnabled()) {
-            if (!m_spk_man->SetupGeneration(true, true)) {
+            if (!m_spk_man->TopUp()) {
                 return false;
             }
         }
