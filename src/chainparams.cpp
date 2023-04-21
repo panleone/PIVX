2dc647148e1ce287e71eb9b3ff547ef050d84706
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2023-04-21 15:41:30 +0200

    Activate v6.0 and shield staking on regtest

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -604,9 +604,8 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_V5_2].nActivationHeight          = 300;
         consensus.vUpgrades[Consensus::UPGRADE_V5_3].nActivationHeight          = 251;
         consensus.vUpgrades[Consensus::UPGRADE_V5_5].nActivationHeight          = 576;
-        consensus.vUpgrades[Consensus::UPGRADE_V6_0].nActivationHeight =
-                Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-        consensus.vUpgrades[Consensus::UPGRADE_SHIELD_STAKING].nActivationHeight = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
+        consensus.vUpgrades[Consensus::UPGRADE_V6_0].nActivationHeight = 600;
+        consensus.vUpgrades[Consensus::UPGRADE_SHIELD_STAKING].nActivationHeight = 601;
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
